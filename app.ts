0d4e58503b531860cd583a34015f882bb09881a3
jerry <jerrynwosu007@gmail.com>
2023-04-30 11:41:47 +0100

    update05

--- app.ts
+++ app.ts
@@ -4,14 +4,11 @@ import path from 'path';
 const app: Application = express();
 const port = process.env.PORT || 9000;
 
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, './client/dist')));
 app.get('*', (_, res: express.Response) => {
-  res.sendFile(
-    path.resolve(__dirname, 'client', 'dist', 'index.html'),
-    (err) => {
-      res.status(500).send(err);
-    }
-  );
+  res.sendFile(path.join(__dirname, './client/dist/index.html'), (err) => {
+    res.status(500).send(err);
+  });
 });
 
 app.get(`/api`, (req: express.Request, res: express.Response) => {
