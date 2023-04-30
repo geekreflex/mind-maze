3b877e216ee7657ab999dc381644afc32477acc9
jerry <jerrynwosu007@gmail.com>
2023-04-30 10:50:35 +0100

    update

---
+++ app.ts
@@ -0,0 +1,23 @@
+import express, { Application } from 'express';
+import path from 'path';
+
+const app: Application = express();
+const port = process.env.PORT || 9000;
+
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.get('*', (_, res: express.Response) => {
+  res.sendFile(
+    path.resolve(__dirname, 'client', 'dist', 'index.html'),
+    (err) => {
+      res.status(500).send(err);
+    }
+  );
+});
+
+app.get(`/api`, (req: express.Request, res: express.Response) => {
+  res.send('API is running...');
+});
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
