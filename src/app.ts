ca2a16273d1ae1f6b91dfd43bbd9c2df2427aa38
jerry <jerrynwosu007@gmail.com>
2023-04-30 09:45:13 +0100

    install client

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import express, { Application } from 'express';
+import path from 'path';
 
 const app: Application = express();
 const port = 9000;
 
-app.get(`/`, (req: express.Request, res: express.Response) => {
-  res.send('Server is up and running');
-});
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+
+  app.get('*', (req: express.Request, res: express.Response) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+} else {
+  app.get(`/`, (req: express.Request, res: express.Response) => {
+    res.send('API is running...');
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
