b80ffc3a2b25585527709b610bce8397a87c9834
jerry <jerrynwosu007@gmail.com>
2023-04-30 10:28:52 +0100

    some updates

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,18 @@ import express, { Application } from 'express';
 import path from 'path';
 
 const app: Application = express();
-const port = 9000;
+const port = process.env.PORT || 9000;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
-
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
   app.get('*', (req: express.Request, res: express.Response) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-} else {
-  app.get(`/`, (req: express.Request, res: express.Response) => {
-    res.send('API is running...');
-  });
 }
+
+app.get(`/`, (req: express.Request, res: express.Response) => {
+  res.send('API is running...');
+});
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
