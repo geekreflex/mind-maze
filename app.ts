32bebbfba46be08b08054ba536aac198712742bb
jerry <jerrynwosu007@gmail.com>
2023-04-30 13:23:00 +0100

    update

--- app.ts
+++ app.ts
@@ -1,20 +1,7 @@
 import express, { Application } from 'express';
-import path from 'path';
 
 const app: Application = express();
 const port = process.env.PORT || 9000;
-
-__dirname = path.resolve();
-
-app.use(express.static(path.join(__dirname, '/client/build')));
-app.get('*', (_, res: express.Response) => {
-  res.sendFile(
-    path.resolve(__dirname, 'client', 'build', 'index.html'),
-    (err) => {
-      res.status(500).send(err);
-    }
-  );
-});
 
 app.get(`/api`, (req: express.Request, res: express.Response) => {
   res.send('API is running...');
